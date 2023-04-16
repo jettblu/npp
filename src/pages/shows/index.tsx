d162d2f031a0a9e96eefe881d9b8fb65e1f3d304
jettblu <jettblu.fabrications@gmail.com>
2023-04-16 19:44:04 -0400

    Add show highlights

--- src/pages/shows/index.tsx
+++ src/pages/shows/index.tsx
@@ -7,6 +7,7 @@ import { nppShows } from "@/shows";
 import { useState } from "react";
 import FeaturedShow from "@/components/shows/FeaturedShow";
 import ShowCard from "@/components/shows/ShowCard";
+import VideoPlayer from "@/components/film/VideoPlayer";
 
 const Members: NextPage = () => {
   const [featuredShow, setFeaturedShow] = useState<IShow>(nppShows[0]);
@@ -35,6 +36,20 @@ const Members: NextPage = () => {
           {archiveShows.map((s) => {
             return <ShowCard show={s} key={s.name} />;
           })}
+        </div>
+        <div className=" mx-auto mt-10 flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4">
+          <div className="flex-grow">
+            <VideoPlayer
+              videoSrc="https://youtu.be/EnYGBnY17V4"
+              isPlaying={false}
+            />
+          </div>
+          <div className="flex-grow">
+            <VideoPlayer
+              videoSrc="https://www.youtube.com/watch?v=29jch1yiPhA"
+              isPlaying={false}
+            />
+          </div>
         </div>
       </main>
     </div>
