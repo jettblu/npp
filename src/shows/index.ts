ee4ad458bd9f3fb5d66e3afb11a403f235db4cef
jettblu <jettblu.fabrications@gmail.com>
2023-04-16 19:07:11 -0400

    Add spring 23 shows

---
+++ src/shows/index.ts
@@ -0,0 +1,39 @@
+import { IShow } from "./models";
+
+export const nppShows: IShow[] = [
+  {
+    name: "Carnival",
+    photoPath: "/shows/spring23/Cabaret.jpg",
+    description:
+      "A classic carnival show full of fun, games, and aquatic shenangingans.",
+    date: "2023-04-14",
+    location: "Baker 2315",
+    time: "12pm",
+  },
+  {
+    name: "Stephen's Magic Show",
+    photoPath: "/shows/spring23/StephenMagic.jpg",
+    description:
+      "A magic show like no other, because it's actually an improv show.",
+    date: "2023-04-14",
+    location: "Studio Theater",
+    time: "8pm",
+  },
+  {
+    name: "Plunder Your Booty",
+    photoPath: "/shows/spring23/Plunder.jpg",
+    description:
+      "Board the Valentine, cross the hub and spoke seas, and find Loverbeard's treasure.",
+    date: "2023-02-18",
+    location: "Studio Theater",
+    time: "8pm",
+  },
+  {
+    name: "Barnyard",
+    photoPath: "/shows/spring23/Barnyard.jpg",
+    description: "Welcome back to the barnyard.",
+    date: "2023-01-27",
+    location: "Studio Theater",
+    time: "8pm",
+  },
+];
