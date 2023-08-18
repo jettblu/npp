2896d85767cd5fd4a08331e813e9be9fed221bae
jettblu <jettblu.fabrications@gmail.com>
2023-08-18 13:31:43 -0700

    Update navbar file structure

--- components/navbars/Navbar.tsx
+++ components/navbars/Navbar.tsx
@@ -1,14 +1,49 @@
+"use client";
+
 import { NextPage } from "next";
+import Link from "next/link";
 
-// wallet SDK helpers
-import NavbarProduction from "./NavbarProduction";
+import Menu, { MenuItem } from "../menu/menu";
+import { usePathname } from "next/navigation";
 
-const Navbar: NextPage = () => {
+export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <NavbarProduction />
-    </div>
-  );
-};
+    <Menu>
+      <MenuItem>
+        <Link href="../about">
+          <span
+            className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-300 dark:hover:text-black transition-colors duration-300 ${
+              pathname == "/about" ? "font-bold" : ""
+            } `}
+          >
+            About
+          </span>
+        </Link>
+      </MenuItem>
 
-export default Navbar;
+      <MenuItem>
+        <Link href="../members">
+          <span
+            className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-300 dark:hover:text-black transition-colors duration-300 ${
+              pathname == "/explore" ? "font-bold" : ""
+            }`}
+          >
+            Members
+          </span>
+        </Link>
+      </MenuItem>
+
+      <MenuItem>
+        <Link href="../shows">
+          <span
+            className={`p-2 lg:px-4 md:mx-2 text-graay-700 dark:text-white md:text-center md:border md:border-solid border-gray-300 dark:border-gray-600 rounded-lg hover:bg-gray-700 dark:hover:bg-white hover:cursor-pointer hover:text-white dark:hover:text-black transition-colors duration-300 mt-1 md:mt-0 md:ml-1`}
+          >
+            Shows
+          </span>
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
+}
